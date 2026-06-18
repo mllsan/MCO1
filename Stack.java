d7e92dba2951af012d6fe0d3502b9dcdea5ca5c6
Raphael Romero <raphael.romero05@aol.com>
2026-06-18 15:19:33 +0800

    implement: stacks

---
+++ Stack.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class Stack {
+    public ArrayList<String> chars;
+    private int size;
+    private String top;
+
+    public Stack() {
+        chars = new ArrayList<String>();
+        this.size = 0;
+        this.top = null;
+    }
+
+    public boolean isEmpty(){
+        return chars.size() == 0;
+    }
+
+    public void push(String elem) {
+        chars.add(0, elem);
+        this.size = chars.size();
+        this.top = chars.get(0);
+    }
+
+    public String pop() {
+        String popped = "-1";
+        if(chars.isEmpty() == false) {
+            popped = chars.get(0);
+            chars.remove(0);
+            this.top = chars.get(0);
+            this.size = chars.size();
+        }
+        return popped;
+    }
+
+    public String top() {
+        String top = "-1";
+
+        if(chars.isEmpty() == false)
+            top = chars.get(0);
+
+        return top;
+    }
+
+    public int getSize() {
+        return this.size;
+    }
+
+
+}
