12b00612d9ba4c754561b2a0116d4d1fe0ddcf87
Raphael Romero <raphael.romero05@aol.com>
2026-06-21 19:39:23 +0800

    create: datasetgenerator class

---
+++ DatasetGenerator.java
@@ -0,0 +1,10 @@
+public class DatasetGenerator {
+
+    // test case 1: hierarchy/chain operators
+
+    // test case 2: nested groupings
+
+    // test case 3: large white spaces, negative results
+
+    // test case 4: invalid operations
+}
