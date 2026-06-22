dd9300d053e8163507154bcd56daeac63aa54464
micah <micahlynn.santos@gmail.com>
2026-06-22 16:05:54 +0800

    implemented boot up of libs
    Refactor timing code to avoid variable redeclaration.

--- Calculator.java
+++ Calculator.java
@@ -9,6 +9,18 @@ public class Calculator {
 
     public static void main(String[] args) throws FileNotFoundException {
 
+        System.out.println("Boot Up Libraries with Example Expression");
+        System.out.println("Infix: 1+2+3+4+5");
+
+        long timeStart = System.nanoTime();
+        String result = convert(new Converter(), "1+2+3+4+5");
+        long timeEnd = System.nanoTime();
+        long duration = timeEnd - timeStart;
+
+        System.out.println(result);
+        System.out.println("Execution Time: " + duration + " nano seconds");
+        System.out.println();
+
         File cases = new File("testcase.txt");
         Scanner input = new Scanner(cases);
 
@@ -17,10 +29,10 @@ public class Calculator {
             System.out.println("Infix: " + line);
 
             // run time analysis
-            long timeStart = System.nanoTime();
-            String result = convert(new Converter(), line);
-            long timeEnd = System.nanoTime();
-            long duration = timeEnd - timeStart;
+            timeStart = System.nanoTime();
+            result = convert(new Converter(), line);
+            timeEnd = System.nanoTime();
+            duration = timeEnd - timeStart;
 
             System.out.println(result);
             System.out.println("Execution Time: " + duration + " nano seconds");
