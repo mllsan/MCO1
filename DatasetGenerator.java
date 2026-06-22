fc9ca1efd2aafed4ce9583d2845ee5a6424a963b
Raphael Romero <raphael.romero05@aol.com>
2026-06-22 15:15:44 +0800

    implement: dataset generator for case 2

--- DatasetGenerator.java
+++ DatasetGenerator.java
@@ -1,8 +1,38 @@
+import java.util.Random;
+
 public class DatasetGenerator {
 
     // test case 1: hierarchy/chain operators
 
     // test case 2: nested groupings
+    public String generateCase2(int tokens) {
+        int nests = tokens / 4;
+
+        if (nests < 1)
+            nests = 1;
+
+        StringBuilder sb = new StringBuilder();
+        Random rand = new Random();
+        int min = 1, max = 9, i, term, nextTerm;
+        String[] operators = {"+","-","*","/","%","^"};
+        String op;
+
+        for (i = 0; i < nests; i++)
+            sb.append("( ");
+
+        term = rand.nextInt((max - min) + 1) + min;
+        sb.append(term).append(" ");
+
+        for (i = 0; i < nests; i++) {
+            op = operators[rand.nextInt(operators.length)];
+            nextTerm = rand.nextInt((max - min) + 1) + min;
+
+            sb.append(op).append(" ").append(nextTerm)
+                    .append(" ").append(") ");
+        }
+
+        return sb.toString().trim();
+    }
 
     // test case 3: large white spaces, negative results
 
