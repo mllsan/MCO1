56653861355def999c4bd22f5b4fe14170c36c59
Raphael Romero <raphael.romero05@aol.com>
2026-06-22 22:29:38 +0800

    fix: added null check

--- Calculator.java
+++ Calculator.java
@@ -10,95 +10,105 @@ public class Calculator {
 
     public static double evaluate(String postfx) {
         Stack<Double> operands = new Stack<>();
-        String[] tokens = postfx.trim().split("\\s+");
         String token;
+        double ans;
 
-        for (int i = 0; i < tokens.length; i++) {
-            token = tokens[i];
+        if (postfx != null) {
+            String[] tokens = postfx.trim().split("\\s+");
+            for (int i = 0; i < tokens.length; i++) {
+                token = tokens[i];
 
-            if (token.equals("^") || token.equals("*") || token.equals("/") ||
-                    token.equals("%") || token.equals("+") || token.equals("-")) {
-                double leftOp, rightOp, result = 0;
+                if (token.equals("^") || token.equals("*") || token.equals("/") ||
+                        token.equals("%") || token.equals("+") || token.equals("-")) {
+                    double leftOp, rightOp, result = 0;
 
-                switch (token.charAt(0)) {
-                    case '^':
-                        rightOp = operands.pop();
-                        leftOp = operands.pop();
-                        result = Math.pow(leftOp, rightOp);
-                        operands.push(result);
-                        break;
-                    case '*':
-                        rightOp = operands.pop();
-                        leftOp = operands.pop();
-                        result = leftOp * rightOp;
-                        operands.push(result);
-                        break;
-                    case '/':
-                        rightOp = operands.pop();
-                        leftOp = operands.pop();
-                        result = leftOp / rightOp;
-                        operands.push(result);
-                        break;
-                    case '%':
-                        rightOp = operands.pop();
-                        leftOp = operands.pop();
-                        result = leftOp % rightOp;
-                        operands.push(result);
-                        break;
-                    case '+':
-                        rightOp = operands.pop();
-                        leftOp = operands.pop();
-                        result = leftOp + rightOp;
-                        operands.push(result);
-                        break;
-                    case '-':
-                        rightOp = operands.pop();
-                        leftOp = operands.pop();
-                        result = leftOp - rightOp;
-                        operands.push(result);
-                        break;
-                    default:
-                        break;
+                    switch (token.charAt(0)) {
+                        case '^':
+                            rightOp = operands.pop();
+                            leftOp = operands.pop();
+                            result = Math.pow(leftOp, rightOp);
+                            operands.push(result);
+                            break;
+                        case '*':
+                            rightOp = operands.pop();
+                            leftOp = operands.pop();
+                            result = leftOp * rightOp;
+                            operands.push(result);
+                            break;
+                        case '/':
+                            rightOp = operands.pop();
+                            leftOp = operands.pop();
+                            result = leftOp / rightOp;
+                            operands.push(result);
+                            break;
+                        case '%':
+                            rightOp = operands.pop();
+                            leftOp = operands.pop();
+                            result = leftOp % rightOp;
+                            operands.push(result);
+                            break;
+                        case '+':
+                            rightOp = operands.pop();
+                            leftOp = operands.pop();
+                            result = leftOp + rightOp;
+                            operands.push(result);
+                            break;
+                        case '-':
+                            rightOp = operands.pop();
+                            leftOp = operands.pop();
+                            result = leftOp - rightOp;
+                            operands.push(result);
+                            break;
+                        default:
+                            break;
+                    }
+                    operands.push(result);
                 }
-                operands.push(result);
+                else
+                    operands.push(Double.parseDouble(token));
             }
-            else
-                operands.push(Double.parseDouble(token));
+            ans = operands.pop();
         }
-        return operands.pop();
+        else
+            ans = -999.999;
+
+        return ans;
     }
 
     public static void main(String[] args) throws FileNotFoundException {
 
-        System.out.println("Boot Up Libraries with Example Expression");
-        System.out.println("Infix: 1+2+3+4+5");
-
-        long timeStart = System.nanoTime();
-        String result = convert(new Converter(), "1+2+3+4+5");
-        long timeEnd = System.nanoTime();
-        long duration = timeEnd - timeStart;
-
+//        System.out.println("Boot Up Libraries with Example Expression");
+//        System.out.println("Infix: 1+2+3+4+5");
+//
+//        long timeStart = System.nanoTime();
+//        String result = convert(new Converter(), "1+2+3+4+5");
+//        long timeEnd = System.nanoTime();
+//        long duration = timeEnd - timeStart;
+//
+//        System.out.println(result);
+//        System.out.println("Execution Time: " + duration + " nano seconds");
+//        System.out.println();
+//
+//        File cases = new File("testcase.txt");
+//        Scanner input = new Scanner(cases);
+//
+//        while (input.hasNextLine()) {
+//            String line = input.nextLine();
+//            System.out.println("Infix: " + line);
+//
+//            // run time analysis
+//            timeStart = System.nanoTime();
+//            result = convert(new Converter(), line);
+//            timeEnd = System.nanoTime();
+//            duration = timeEnd - timeStart;
+//
+//            System.out.println(result);
+//            System.out.println("Execution Time: " + duration + " nano seconds");
+//            System.out.println();
+//        }
+//        input.close();
+        DatasetGenerator g = new DatasetGenerator();
+        double result = evaluate(convert(new Converter(), "(1 + 2]"));
         System.out.println(result);
-        System.out.println("Execution Time: " + duration + " nano seconds");
-        System.out.println();
-
-        File cases = new File("testcase.txt");
-        Scanner input = new Scanner(cases);
-
-        while (input.hasNextLine()) {
-            String line = input.nextLine();
-            System.out.println("Infix: " + line);
-
-            // run time analysis
-            timeStart = System.nanoTime();
-            result = convert(new Converter(), line);
-            timeEnd = System.nanoTime();
-            duration = timeEnd - timeStart;
-
-            System.out.println(result);
-            System.out.println("Execution Time: " + duration + " nano seconds");
-            System.out.println();
-        }
-        input.close();
     }
 }
